e173d17aef3d4005df987de101922f67b8f5cdf2
Ojaswi <ojaswideep2020@gmail.com>
2025-05-28 11:07:00 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,65 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MainApp());
 }
-
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('AUR BTAO KYA HAAL CHAAL, avi madam')
+        backgroundColor: Colors.black,
+       /* appBar: AppBar(
+          title: const Text("Lets See", style: TextStyle(color: Colors.lightGreenAccent, fontFamily: "sans-serif")),
+          centerTitle: true,
+          leading: Icon(Icons.menu, color: Colors.lightGreenAccent, size: 24),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.logout, color: Colors.lightGreenAccent, size: 20),
+            )
 
+          ],
+          backgroundColor: Colors.black,
+        )
+        ,*/
+        body: ListView(
+          scrollDirection: Axis.horizontal,
+
+          children:  [
+
+            Container(
+              width: 250,
+              color: Colors.green[500],
+              /*borderRadius: BorderRadius.circular(40),*/
+            ),
+
+
+            /*child: Text("Hello, There !!!", style: TextStyle(fontSize: 28)),*/
+
+
+
+
+             Container(
+               width: 250,
+
+                  color: Colors.green[300],
+
+
+
+              /*child: Text("Hello, There !!!", style: TextStyle(fontSize: 28)),*/
+
+            ),
+
+
+              Container(
+
+                width: 400,
+                  color: Colors.green[200],
+              /*child: Text("Hello, There !!!", style: TextStyle(fontSize: 28)),*/
+            ),
+        ]
         ),
       ),
     );
